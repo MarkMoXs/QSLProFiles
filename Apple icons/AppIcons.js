9f71c1dd2958db0d65d5f04125036d55ac41ee68
MarkMoXs <wanmeivisual@live.com>
2026-02-09 15:18:19 +0800

    add app icons

---
+++ Apple icons/AppIcons.js
@@ -0,0 +1,99 @@
+{
+    "name"; "QSLProFiles Icons";
+    "description"; "Icons for Loon/QX rules";
+    "time"; "2025-02-09";
+    "icons"; [
+        {
+            "name": "Amazon",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Amazon.png"
+        },
+        {
+            "name": "Apple",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Apple.png"
+        },
+        {
+            "name": "Bing",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Bing.png"
+        },
+        {
+            "name": "Cloudflare",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Cloudflare.png"
+        },
+        {
+            "name": "Discord",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Discord.png"
+        },
+        {
+            "name": "Facebook",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Facebook.png"
+        },
+        {
+            "name": "Gemini",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Gemini.png"
+        },
+        {
+            "name": "GitHub",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/GitHub.png"
+        },
+        {
+            "name": "Google",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Google.png"
+        },
+        {
+            "name": "iCloud",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/iCloud.png"
+        },
+        {
+            "name": "Instagram",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Instagram.png"
+        },
+        {
+            "name": "Microsoft",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Microsoft.png"
+        },
+        {
+            "name": "Netflix",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Netflix.png"
+        },
+        {
+            "name": "OpenAI",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/OpenAI.png"
+        },
+        {
+            "name": "Reddit",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Reddit.png"
+        },
+        {
+            "name": "Speedtest",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Speedtest.png"
+        },
+        {
+            "name": "Spotify",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Spotify.png"
+        },
+        {
+            "name": "Telegram",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Telegram.png"
+        },
+        {
+            "name": "TikTok",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/TikTok.png"
+        },
+        {
+            "name": "Twitch",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Twitch.png"
+        },
+        {
+            "name": "Twitter",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Twitter.png"
+        },
+        {
+            "name": "YouTube",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/YouTube.png"
+        },
+        {
+            "name": "Global",
+            "url": "https://raw.githubusercontent.com/MarkMoXs/QSLProFiles/refs/heads/TestFiles/icon/Global.png"
+        }
+    ]
+}
